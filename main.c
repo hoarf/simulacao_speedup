21ad8a7a921c8beb43fed2a55eee1f126596167d
Alan <alan.ficagna@gmail.com>
2013-06-02 17:52:15 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,14 @@
 int main(int argc, char const *argv[])
 {
 	int  i;
+	
+	//Exemplo de como usar o timer
 	timer_reset();
 	timer_begin_counting();
 	for (i = 0; i < 1000000; ++i)
-		printf("%d", i);	
+		printf("%d", i);
+		
+	//Exemplo de como imprimir o resultado do timer		
 	timer_stop_counting();
 	return 0;
 }
