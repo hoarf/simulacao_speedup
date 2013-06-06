6234451e3a458f4c0e745181d26d05faa1566f08
Nicolas Kagami <nicolaskagami@gmail.com>
2013-06-06 17:01:48 -0300

    Create schedule.sh
    Tem a explicação de como usar dentro

---
+++ schedule.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Scheduler - Nicolas Silveira Kagami
+# How to use: bash scheduler.sh 
+
+OUTPUT_FILE="Results.csv"
+EXE_NAME="./broadcast"
+LIST=`echo "$1,"`
+SAFE="1"
+i=1
+
+while [ $SAFE != "0" ]
+do
+    PROC_NUM=`echo "$LIST" | cut -d, -f${i}`
+    if [[ "$PROC_NUM" =~ ^[0-9]+$ ]]  
+    then
+        BEG_TIME=`date +'%s.%N'`
+        mpirun -np $PROC_NUM $EXE_NAME >/dev/null
+        END_TIME=`date +'%s.%N'`
+        RUN_TIME=`bc <<< "scale=9;$END_TIME - $BEG_TIME"`
+        echo "$PROC_NUM,$RUN_TIME" >> $OUTPUT_FILE
+    else
+        SAFE="0"
+    fi
+    let "i=i+1"
+done
