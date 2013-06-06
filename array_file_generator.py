9e67dcb65d062d40f9c5ca61c611abca2bdf54fd
Alan <alan.ficagna@gmail.com>
2013-06-06 13:51:54 -0300

    troquei o gerador pra gerar um arquivo de testes mais sano

--- array_file_generator.py
+++ array_file_generator.py
@@ -2,7 +2,7 @@ import random
 
 f = open('array.txt', 'w')
 
-for i in xrange(0,9999999):
-    f.write(str(random.randint(0, 99)))
+for i in xrange(0,50):
+    f.write(str(random.randint(0, 99)) + ' ')
 
 f.close
